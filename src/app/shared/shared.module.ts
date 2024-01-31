46bb556b7506c00d082e276550a32387ce0a6bf0
veeraj2404 <156992912+veeraj2404@users.noreply.github.com>
2024-01-31 15:56:58 +0530

    Add files via upload

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { StarComponent } from "./star.component";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+    declarations: [
+        StarComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        CommonModule,
+        FormsModule,
+        StarComponent
+    ]
+})
+export class SharedModule { }
